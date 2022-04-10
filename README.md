b4057fa423659077a5ccde708211b8c52ef0a787
cepthomas <cepthomas@gmail.com>
2022-04-10 13:22:35 -0500

    More functionality.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # SbotLogger
 
 Intercepts the console write and copies to a file. Adds optional timestamp.
+Examines the first word to identify as a user supplied category.
 
 Simple categories by prefacing message with meaningful strings. If they appear in
-notify_cats, a dialog is presented.
-
+`notify_cats`, a dialog is presented. Those in `ignore_cats` are ignored.
 
 ## Commands
 None
@@ -15,5 +15,6 @@ None
 | :--------          | :-------                        | :------   |
 | `file_path`        | Full path to the log file       | if empty default is `%data_dir%\Packages\User\SbotStore\sbot.log`  |
 | `mode`             | File add mode                   | `append` OR `clean` (on start) OR `off` |
-| `time_format`      | How to format (strftime)        | `strftime format` OR empty (no timestamp) |
-| `notify_cats`      | Notify user if text begins with | comma separated strings |
+| `time_format`      | How to format (strftime)        | `strftime format` OR empty (default timestamp) OR null (no timestamp) |
+| `ignore_cats`      | Ignore these user categories    | comma separated strings |
+| `notify_cats`      | Notify user if in categories    | comma separated strings |
--- sbot_logger.py
+++ sbot_logger.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from datetime import datetime
 import sublime
 
 
@@ -11,7 +12,6 @@ _logger = None
 def plugin_loaded():
     # This should only be called once per ST instance.
     global _logger
-
     _logger = SbotLogger()
     _logger.start()
 
@@ -19,65 +19,81 @@ def plugin_loaded():
 #-----------------------------------------------------------------------------------
 def plugin_unloaded():
     global _logger
-
     _logger.stop()
-    
+
 
 #-----------------------------------------------------------------------------------
 class SbotLogger():
 
-    _prev_stdout = None
-    _prev_stderr = None
+    _console_stdout = None
+    _console_stderr = None
     _log_fn = None
     _time_format = None
     _mode = None
-    _notifs = []
+    _ignore_cats = None
+    _notif_cats = None
 
     def start(self):
-        if self._prev_stdout is None:
-            self._prev_stdout = sys.stdout
-            self._prev_stderr = sys.stderr
-            sys.stdout = self
-            sys.stderr = self
-        
-            settings = sublime.load_settings("SbotLogger.sublime-settings")
+        if self._console_stdout is None:
+            try:
+                # Get the settings.
+                settings = sublime.load_settings("SbotLogger.sublime-settings")
+                self._mode = settings.get('mode')
+                fp = settings.get("file_path")
+                self._log_fn = fp if len(fp) > 0 else os.path.join(sublime.packages_path(), 'User', 'SbotStore', 'sbot.log')
+                self._notif_cats = settings.get('notify_cats')
+                self._ignore_cats = settings.get('ignore_cats')
+                self._time_format = settings.get('time_format')
 
-            self._notifs = settings['notify_cats'].split(',')
+                # Clean old log maybe.
+                if self._mode == 'clean':
+                    with open(self._log_fn, "w") as log:
+                        pass
 
-            if settings['mode'] != 'off':
-                self._mode = settings['mode']
-                if len(settings["file_path"]) > 0:
-                    self._log_fn = settings["file_path"] 
-                else:
-                    self._log_fn = os.path.join(sublime.packages_path(), 'User', 'SbotStore', 'sbot.log')
-
-            if len(settings['time_format']) > 0:
-                self._time_format = settings['time_format']  # TODO msec. Doesn't like .%f.
-
-            # Clean old log maybe.
-            if self._mode == 'clean':
-                with open(self._log_fn, "w") as log:
-                    pass
+                print("INF Stealing stdout and stderr!")
+                self._console_stdout = sys.stdout
+                self._console_stderr = sys.stderr
+                sys.stdout = self
+                sys.stderr = self
+                print("INF Stolen stdout and stderr!")
+            except Exception as e:
+                logging.exception(e)
 
     def stop(self):
-        if self._prev_stdout is not None:
-            sys.stdout = self._prev_stdout
-            sys.stderr = self._prev_stderr
+        if self._console_stdout is not None:
+            sys.stdout = self._console_stdout
+            sys.stderr = self._console_stderr
+            print("INF Restored stdout and stderr!")
 
     def write(self, message):
+        # Write one.
+
         cmsg = message.rstrip()
         if len(cmsg) > 0:
-            if self._time_format is not None:
-                outmsg = f'{time.strftime(self._time_format, time.localtime())} {cmsg}\n'
-            else:
-                outmsg = f'{cmsg}\n'
+            # Dig out possible category.
+            cat = None
+            parts = cmsg.split(' ')
+            if len(parts) > 0:
+                cat = parts[0]
 
-            self._prev_stdout.write(outmsg)
-            self._prev_stdout.flush()
+            if cat not in self._ignore_cats:
+                # Format time.
+                if self._time_format is None:
+                    outmsg = f'{cmsg}\n'
+                elif len(self._time_format) > 0:
+                    outmsg = f'{time.strftime(self._time_format, time.localtime())} {cmsg}\n'
+                else:
+                    time_str = f'{str(datetime.now())}'[0:-3]
+                    outmsg = f'{time_str} {cmsg}\n'
 
-            if self._mode is not None:
-                with open(self._log_fn, "a") as log:
-                    log.write(outmsg)
+                # Always write to console.
+                self._console_stdout.write(outmsg)
+                self._console_stdout.flush()
 
-            if cmsg.split(' ')[0] in self._notifs:
-                sublime.message_dialog(cmsg)
+                # Maybe write to file.
+                if self._mode is not None:
+                    with open(self._log_fn, "a") as log:
+                        log.write(f'{cat} {outmsg}')
+
+                if cat in self._notif_cats:
+                    sublime.message_dialog(cmsg)
